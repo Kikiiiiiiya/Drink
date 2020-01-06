f04108a19a98af2b8f26f79d16080f4bd37012e3
Kikiiiiiiya <2027555278@qq.com>
2020-01-07 03:05:04 +0800

    new files

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -35,23 +35,23 @@
 export default {
   data () {
     return {
-      title_name: "热门",
+      title_name: "热门店铺",
       grids: [
-        {title:"外卖",image:"/static/images/1.png"},
-        {title:"零食",image:"/static/images/2.png"},
-        {title:"主食",image:"/static/images/3.png"},
-        {title:"面食",image:"/static/images/4.png"}
+        {title:"饮料",image:"/static/images/1.jpg"},
+        {title:"牛奶",image:"/static/images/2.jpg"},
+        {title:"咖啡",image:"/static/images/3.jpg"},
+        {title:"茶",image:"/static/images/4.jpg"}
       ],
       top: [
-        {name:"店名1",address:"地址1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍1"},
-        {name:"店名2",address:"地址2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍2"},
-        {name:"店名3",address:"地址3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍3"},
-        {name:"店名4",address:"地址4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",remark:"介绍4"}
+        {name:"百事",address:"徐东大街梨园小路",image:"http://img.zcool.cn/community/0174c2554358260000019ae9885e6d.jpg@1280w_1l_2o_100sh.png",remark:"百事，新一代的选择"},
+        {name:"旺仔牛奶",address:"暂无实体店，敬请期待",image:"https://graph.baidu.com/resource/1215b70fb72bac5b6e5cb01578328800.jpg",remark:"在看我？在看我？还在看我？再看，我就把你喝掉！"},
+        {name:"星巴克",address:"和平大道730号村福星惠誉水岸国际三号楼一层",image:"https://graph.baidu.com/thumb/v2/120462370,707526853.jpg",remark:"认真对待每一位顾客，一次只烹调顾客那一杯咖啡"},
+        {name:"奈雪的茶",address:"徐东大街120号群星城1F层",image:"http://img.mp.itc.cn/upload/20170620/7bc26b2c66f0499f97392cf644255543_th.jpg",remark:"高山云雾出好茶，明前绿茶，叶子嫩的可以直接吃"}
       ],
       imgUrls: [
-        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+        'https://graph.baidu.com/resource/12125069a2e75995348a701578328977.jpg',
+        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4184875832,632365737&fm=26&gp=0.jpg',
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=624279544,4142658441&fm=26&gp=0.jpg'
       ],
       indicatorDots: false,
       autoplay: false,
